71ee7b9d1c9fe97700b06841200567b8c4ba23cd
orhan <orhanu2020@gmail.com>
2020-12-08 19:56:29 -0500

    Java Implementation for Armstrong Numbers

---
+++ src/simple/ArmstrongNumbers.java
@@ -0,0 +1,37 @@
+package simple;
+
+import java.util.stream.IntStream;
+
+/**
+ * https://www.geeksforgeeks.org/program-for-armstrong-numbers/
+ */
+public class ArmstrongNumbers {
+    public static void main(String[] args) {
+        System.out.println(isArmstrong(153));
+        System.out.println(isArmstrong(153));
+        System.out.println(getNthArmstrong(12));
+    }
+
+    private static boolean isArmstrong(int number) {
+        String numberAsString = String.valueOf(number);
+        int size = numberAsString.length();
+
+        int sum = 0;
+        for(char ch : numberAsString.toCharArray()) {
+            int characterAsInt = Integer.parseInt(ch+"");
+            sum += Math.pow(characterAsInt, size);
+        }
+        return sum == number;
+    }
+
+    private static int getNthArmstrong(int n) {
+        int count = 0;
+        for(int i = 1; i < Integer.MAX_VALUE; i++) {
+            if(isArmstrong(i) == true) {
+                count++;
+                if(count == n) { return i; }
+            }
+        }
+        return count;
+    }
+}
